17fb5e93557d70506581cedb7a52d8190f999b01
Justin W Soto <soto@pdx.edu>
2021-09-15 16:49:40 -0700

    one failing test down

--- __tests__/simpleDb.test.js
+++ __tests__/simpleDb.test.js
@@ -1,4 +1,5 @@
 //import some things
+
 import { rm, mkdir } from 'fs/promises';
 import { SimpleDb } from '../lib/simpleDb.js';
 
@@ -8,15 +9,17 @@ describe('simpleDb', () => {
 
   beforeEach(() => {
     return rm(rootDir, { force: true, recursive: true }).then(() => {
-      return mkdir(rootDir, { recursive: true });
+      return mkdir(rootDir);
     });
   });
 
-  it('should give a unique id to an object and json stringify it', () => {
-    const saveIt = new SimpleDb(rootDir);
-
-    return saveIt;
-      
-  
+  it('should give a unique id to an object and stringify it', () => {
+    const simpleDb = new SimpleDb(rootDir);
+    const blobject = simpleDb.save({ data: 'some data' }).catch((err) => {
+      console.error(err.message());
+    });
+    expect(blobject).toBe(expect.any(String));
   });
+
 });
+
